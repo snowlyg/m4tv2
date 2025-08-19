442698de5d448a9cb8720f385a140b64cd86bb85
snowlyg <brendenaudrina6287@gmail.com>
2025-08-19 14:29:05 +0800

    update sh script

--- download.sh
+++ download.sh
@@ -8,6 +8,7 @@ apt install wget git  build-essential -y
 cd /app
 git clone https://github.com/facebookresearch/seamless_communication.git --depth 1
 cd seamless_communication
+# cd /app/seamless_communication
 
 pip install . -i https://pypi.tuna.tsinghua.edu.cn/simple/
 cd demo/m4tv2
